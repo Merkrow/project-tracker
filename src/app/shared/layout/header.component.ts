92f655dab973752eec1fd3ffc1b1e42448f73627
Sergey Pliev <merkrow@gmail.com>
2017-08-08 16:43:14 +0300

    header markup and some fixes

--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -5,7 +5,8 @@ import { UserService } from '../services';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   constructor(
